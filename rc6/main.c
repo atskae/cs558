cdcf2dbad3fc0603a3d32421a7ae4493918965af
Atsuko Shimizu <ashimiz1@cs.binghamton.edu>
2018-09-25 09:55:15 -0400

    Wrote key schedule and started encryption... incorrect results though

--- rc6/main.c
+++ rc6/main.c
@@ -25,16 +25,18 @@ unsigned char* text = NULL; // text to encrypt or decrypt
 int text_size = 0;
 
 // Four 32-bit registers for encryption/decryption
-uint32_t reg_A = 0x00000000;
-uint32_t reg_B = 0x00000000;
-uint32_t reg_C = 0x00000000;
-uint32_t reg_D = 0x00000000;
+#define REG_A 0
+#define REG_B 1
+#define REG_C 2
+#define REG_D 3
+uint32_t regs[4] = { 0x00000000, 0x00000000, 0x00000000, 0x00000000};
 
 // Key schedule for RC6
 uint32_t round_keys[2*NUM_ROUNDS + 4]; // S array from paper
 uint32_t* L; // user-key with zero-byte padding in array format, little-endian format
-uint32_t P32 = 0xB7E15163; // "magical constant"
-uint32_t Q32 = 0x9E3779B9; // "magical constant"
+int L_size;
+const uint32_t P32 = 0xB7E15163; // "magical constant"
+const uint32_t Q32 = 0x9E3779B9; // "magical constant"
 
 
 /*
@@ -57,7 +59,7 @@ void print_uint(char* name, uint32_t* ptr, int num_elem) {
 
 	printf("%s (%i %i-byte elements): ", name, num_elem, WORD_SIZE/8);
 	for(int i=0; i<num_elem; i++) {
-		printf("%04x ", (unsigned char) ptr[i]);
+		printf("%08x ", ptr[i]);
 	}
 	printf("\n");
 
@@ -123,36 +125,128 @@ void parse_input(char* file) {
 	memset(key, 0, 256);
 	key_size = 0;
 		
-	L = (uint32_t*) malloc(sizeof(256)/sizeof(uint32_t)); // holds groups of bytes of size WORD_SIZE in little-endian format, with the zero padding
-	int L_size = 0;
-	memset(L, 0, 256);
+	L = (uint32_t*) malloc(256 * sizeof(uint32_t)); // holds groups of bytes of size WORD_SIZE in little-endian format, with the zero padding
+	memset(L, 0, 256*sizeof(uint32_t) );
 	uint32_t mask;
 	// read each byte
 	for(int i=0; i<hex_string_size/2; i++) {
 		sscanf(pos, "%2hhx", &key[i]);
         	mask = 0x00000000; // reset mask
-		mask |= key[i]; // mask = 0x 00 00 00 <key>
-		mask << 8 * (i%4); // mask = 0x00 <key> 00 00 ; shift left 8*2 = 16 times
-		L[i % 4] |= mask; // L[pos] = 0x<b2> <key> <b1> <b0>
+		mask |= key[i]; // mask = 0x 00 00 00 <key>	
+		mask = mask << (8 * (i%4)); // mask = 0x00 <key> 00 00 ; shift left 8*2 = 16 times
+	//	printf("mask: %08x (val %02x)\n", mask, key[i]);
+		L[ (int)floor(i/4) ] |= mask; // L[pos] = 0x<b2> <key> <b1> <b0>
+	//	printf("L[%i] = %08x\n", (int)floor(i/4), L[(int)floor(i/4)] );
 		pos += 2;
 		key_size++;
 	}
 
 	fclose(input);
-	L_size = ceil(key_size/(WORD_SIZE/8));
-	
+	L_size = ceil((float)key_size/(WORD_SIZE/8));
+		
 	// debug
 	print_bytes("Text", text, text_size);
 	print_bytes("Key", key, key_size);
 	print_uint("L", L, L_size);
 }
 
+// shifts a to the left by the amount given by the least significant log2(WORD_SIZE) bits
+uint32_t shiftl(uint32_t a, uint32_t b) {
+
+	int s = b & ~(0xFFFFFE00); // extract the log2(WORD_SIZE) = 5 least significant bits
+	//printf("a: %08x, b: %08x, shift a to the left by %i (%06x)\n", a, b, s, s);
+	return a << s;
+}
+
 void run_key_schedule() {
 
 	printf("run_key_schedule()\n");
 
-	printf("run_key_schedule() completed.\n");
+	memset(round_keys, 0, (2*NUM_ROUNDS + 4)*sizeof(uint32_t));	
+	round_keys[0] = P32;
+	
+	for(int i=1; i<=2*NUM_ROUNDS+3; i++) {
+		round_keys[i] = round_keys[i-1] + Q32;
+	}
+
+	int i=0, j=0;
+	regs[REG_A] = 0x00000000;
+	regs[REG_B] = 0x00000000;
+
+	int max = 2*NUM_ROUNDS + 4;
+	if(L_size > max) max = L_size;
+	int v = 3 * max;
+
+	for(int s=1; s<=v; s++) {
+	
+		round_keys[i] = shiftl(round_keys[i] + regs[REG_A] + regs[REG_B], 3);
+		regs[REG_A] = round_keys[i];
+		
+		L[j] = shiftl(L[j] + regs[REG_A] + regs[REG_B], regs[REG_A] + regs[REG_B]);
+		regs[REG_B] = L[j];
+	
+		i = (i+1) % (2*NUM_ROUNDS + 4);
+		j = (j+1) % L_size;
+
+	}
+
+	// reset the values
+	regs[REG_A] = 0x00000000;
+	regs[REG_B] = 0x00000000;
+
+	//print_uint("After key schedule", round_keys, 2*NUM_ROUNDS + 4);
+	//printf("run_key_schedule() completed.\n");
 }
+
+void partition(unsigned char* text) {
+	uint32_t mask;
+	int wb = WORD_SIZE/8;
+	for(int i=0; i<text_size; i++) { // for each byte in the text
+		mask = 0x00000000; // reset mask
+		mask |= text[i]; // mask = 0x 00 00 00 <b>	
+		mask = mask << (8 * (i % wb)); // mask = 0x00 <b> 00 00 ; shift left 8*2 = 16 times
+		regs[ (int)floor(i/wb) ] |= mask; // regs[r] = 0x<b2> <b> <b1> <b0>
+	}
+
+	print_uint("Registers after partition text", regs, 4);	
+}
+
+void encrypt() {
+	printf("encrypt()\n");
+
+	regs[REG_B] = regs[REG_B] + round_keys[0];
+	regs[REG_D] = regs[REG_D] + round_keys[1];
+
+	int logw = log2(WORD_SIZE);
+	for(int i=1; i<=NUM_ROUNDS; i++) {
+		uint32_t t = shiftl(regs[REG_B] * (2*regs[REG_B] + 1), logw);
+		uint32_t u = shiftl(regs[REG_D] * (2*regs[REG_D] + 1), logw);
+		regs[REG_A] = ( shiftl(regs[REG_A] ^ t, u) ) + round_keys[2*i];
+		regs[REG_C] = ( shiftl(regs[REG_C] ^ u, t) + round_keys[2*i + 1]);
+
+		uint32_t old_regs[4];
+		old_regs[REG_A] = regs[REG_A];
+		old_regs[REG_B] = regs[REG_B];
+		old_regs[REG_C] = regs[REG_C];
+		old_regs[REG_D] = regs[REG_D];
+
+		regs[REG_A] = old_regs[REG_B];
+		regs[REG_B] = old_regs[REG_C];
+		regs[REG_C] = old_regs[REG_D];
+		regs[REG_D] = old_regs[REG_A];
+	}
+
+	regs[REG_A] = regs[REG_A] + round_keys[2*NUM_ROUNDS + 2];
+	regs[REG_C] = regs[REG_C] + round_keys[2*NUM_ROUNDS + 3];
+
+	print_uint("Encrypted", regs, 4);
+}
+
+
+void decrypt() {
+	printf("decrypt()\n");
+}
+
 
 int main(int argc, char* argv[]) {
 
@@ -165,6 +259,10 @@ int main(int argc, char* argv[]) {
 	parse_input(argv[1]); // obtain text and user key	
 
 	run_key_schedule(); // generates 2r + 4 keys in round_keys[]
+	partition(text); // partition plaintext or cipher text into the 4 registers in little-endian format	
 	
+	if(action == ENCRYPT) encrypt();
+	else decrypt();
+
 	return 0;
 }
